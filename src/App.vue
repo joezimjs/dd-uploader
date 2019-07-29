481ea649c149c618076d3a5d1ea62de599c16f41
Joseph Zimmerman <contact@joezimjs.com>
2019-07-29 07:08:56 -0500

    Starting

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<DDUploader class="drop-area">
+			<template v-slot:default="{ onInputChange, files }">
+				<input type="file" @change="onInputChange" />
+				<div v-for="(file, i) of files" :key="i">
+					{{ file.name }}
+				</div>
+			</template>
+		</DDUploader>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DDUploader from './components/DDUploader.vue'
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+	name: 'app',
+	components: { DDUploader }
+}
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.drop-area {
+	padding: 50px;
+	background: #f0f8ff;
+}
+
+.drop-area[data-active=true] {
+	border: 2px dashed black;
 }
 </style>
---
+++ src/components/DDUploader.vue
@@ -0,0 +1,28 @@
+<template>
+	<div data-active="true">
+		<slot v-bind="{ doSomething, files, onInputChange }">
+			<input type="file" @change="onInputChange">
+		</slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DDUploader',
+	data () {
+		return {
+			files: []
+		}
+	},
+	methods: {
+		onInputChange (e) {
+			this.files.push(...e.target.files)
+			e.target.value = null
+			console.log(this.files)
+		},
+		doSomething () {
+			return 'yo dawg'
+		}
+	}
+}
+</script>
